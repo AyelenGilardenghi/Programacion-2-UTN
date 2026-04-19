64f573f15e9e2f8f383d671bd129ba1a28d7b3c3
Ayelen Gilardenghi <ayelenyasmin@gmail.com>
2026-04-19 18:44:36 -0300

    terminado ejercicio 10

---
+++ OneDrive/Documentos/Programacion 2-UTN/GUIAS/ejercicios-guia1/src/ejercicio_10/guia1/Test.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_10.guia1;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+/**
+ *
+ * @author Gilar
+ */
+public class Test {
+    public static void main(String[] args) {
+        Domicilio domicilioP1 = new Domicilio("plaza", 818,"illa ortuzar");
+        
+        LocalDate fechaNacimientoC1 = LocalDate.of(1991, Month.MARCH, 7);
+        LocalDate fechaAperturaC1 = LocalDate.of(2026, Month.JANUARY, 5);
+        Persona p1= new Persona("Juan", "perez", fechaNacimientoC1, domicilioP1);
+        CuentaBancaria c1= new CuentaBancaria(TipoCuentaBancaria.CAJA_DE_AHORRO, p1, fechaAperturaC1);
+   
+        System.out.println(c1.toString());
+    }
+}
