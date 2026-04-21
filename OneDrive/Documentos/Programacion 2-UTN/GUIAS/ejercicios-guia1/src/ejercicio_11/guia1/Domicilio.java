2ed0e960f1a595f75a10c394177081f5077869f8
Ayelen Gilardenghi <ayelenyasmin@gmail.com>
2026-04-21 13:38:15 -0300

    ejer 11 sin terminar

---
+++ OneDrive/Documentos/Programacion 2-UTN/GUIAS/ejercicios-guia1/src/ejercicio_11/guia1/Domicilio.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_11.guia1;
+
+
+
+/**
+ *
+ * @author Gilar
+ */
+public class Domicilio {
+    private String calle;
+    private int altura;
+    private String barrio;
+
+    public Domicilio(String calle, int altura, String barrio) {
+        setCalle(calle);
+        setAltura(altura);
+        setBarrio(barrio);
+    }
+
+    public void setCalle(String calle) {
+        if( calle != null){
+            this.calle = calle;
+        }
+    }
+
+    public void setAltura(int altura) {
+        if(altura >= 0){
+            this.altura = altura;
+        }
+    }
+
+    public void setBarrio(String barrio) {
+        if(barrio !=null){
+            this.barrio = barrio;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Domicilio: " +  calle + ", " + altura + ", " + barrio+".";
+    }
+    
+    
+
+}
---
+++ OneDrive/Documentos/Programacion 2-UTN/GUIAS/ejercicios-guia1/src/ejercicio_11/guia1/TipoCuentaBancaria.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_11.guia1;
+
+
+/**
+ *
+ * @author Gilar
+ */
+enum TipoCuentaBancaria {
+    CAJA_DE_AHORRO,
+    CUENTA_CORRIENTE;
+}
